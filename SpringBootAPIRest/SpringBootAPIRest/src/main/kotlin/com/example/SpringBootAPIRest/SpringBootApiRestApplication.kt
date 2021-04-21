cedee41da931df55ba6f21bf2524cec515d9b909
DaianedaSilva <daianedasilva044@gmail.com>
2021-04-21 15:16:42 -0300

    Criando SPringBoot

---
+++ SpringBootAPIRest/SpringBootAPIRest/src/main/kotlin/com/example/SpringBootAPIRest/SpringBootApiRestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.SpringBootAPIRest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootApiRestApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootApiRestApplication>(*args)
+}
